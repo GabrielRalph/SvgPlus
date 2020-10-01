8ae8e09fbd1e0da75c24a342b064b99d7da80219
GabrielRalph <gltralph@gmail.com>
2020-10-01 13:41:09 +1000

    vectir

--- load.js
+++ load.js
@@ -1,5 +1,5 @@
 var vector_script = document.createElement("script");  // create a script DOM node
-vector_script.src = 'https://www.svg.plus/vectors.js';  // set its src to the provided URL
+vector_script.src = 'https://www.svg.plus/vector.js';  // set its src to the provided URL
 document.head.appendChild(vector_script);
 var svg_plus_script = document.createElement("script");  // create a script DOM node
 svg_plus_script.src = 'https://www.svg.plus/SvgPlus.js';  // set its src to the provided URL
